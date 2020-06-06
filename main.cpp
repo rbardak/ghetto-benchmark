4b82864b33d47bae9ced31ee4e32e9f75d906e48
Robin Hobil Bardakcioglu <robin.hobil@gmail.com>
2020-06-06 19:53:13 +0200

    implemented a least squares benchmark

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 #include <iomanip>
 #include <functional>
 
+#include "matrix_test.hpp"
 #include "sin_test.hpp"
 #include "util.hpp"
 
 int main()
 {
-    std::cout << "Performing sine wave benchmark...\n";
+    std::cout << "Performing sine wave benchmark. Pure CPU performance.\n";
     printSeconds(sinusTest(100000000));
+    std::cout << "Performing least squares benchmark. Mostly RAM performance.\n";
+    printSeconds(MatrixBenchmarks::leastSquaresPolynomial<100>(500000));
+
     return 0;
 }
---
+++ matrix_test.hpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include "eigen/Eigen/Dense"
+
+#include "timing.hpp"
+
+class MatrixBenchmarks
+{
+private:
+    using Matrix = Eigen::MatrixXd;
+    using Vector = Eigen::VectorXd;
+
+public:
+    template <long order>
+    static double leastSquaresPolynomial(long size)
+    {
+        Vector input = Vector::LinSpaced(size, -1, 1);
+        Vector response = Vector::Random(size);
+        Matrix expansionMatrix{size, order + 1};
+        
+        timing::Measurement timer;
+        timer.start();
+        
+        for(long i = 0; i < size; ++i)
+        {
+            Vector expansion = calcPolynomialExpansion<order>(input[i]);
+            expansionMatrix.row(i) = expansion;
+        }
+        
+        // Using these coefficients in a polynomial series will yield the 
+        // polynomial function with minimal least squares deviation from the
+        // input/response data.
+        // Volatile is used to insure that the compiler does not skip the
+        // benchmark, due to its results never being used.
+        volatile auto expansionCoefficients = 
+            expansionMatrix.colPivHouseholderQr().solve(response);
+        
+        timer.end();
+        return timer.elapsedTime();
+    }
+
+    template <long order>
+    static Vector calcPolynomialExpansion(double input)
+    {
+        Vector expansion{order + 1};
+        
+        for(long i = 0; i < order + 1; ++i)
+            expansion[i] = std::pow(input, i);
+
+        return expansion;
+    }
+};
