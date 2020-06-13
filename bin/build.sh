882c8a4ecfebf7c8bbeb031568ffa86dbe49c843
Robin Hobil Bardakcioglu <robin.hobil@gmail.com>
2020-06-13 15:29:13 +0200

    build script

---
+++ bin/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_DIR=$SCRIPT_DIR/../
+BUILD_DIR=$PROJECT_DIR/build/$(hostname)/
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+cmake $PROJECT_DIR
+make -j8
