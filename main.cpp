c383b3e1637791b24f0f596bccef55bbab6226b5
Robin Hobil Bardakcioglu <robin.hobil@gmail.com>
2020-06-13 18:14:48 +0200

    minor cleanuo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <iomanip>
-#include <functional>
 
 #include "matrix_test.hpp"
 #include "memcpy_test.hpp"
@@ -19,6 +18,7 @@ int main()
     size_t memSize = 512l * 1000l * 1000l;
     size_t memPasses = 100;
     double memcpySeconds = memcpy_test(memSize, memPasses);
+
     printSeconds(memcpySeconds);
     double GBPerSecond = static_cast<double>(memSize * memPasses) / memcpySeconds / 1000000000;
     std::cout << std::setprecision(4) << "Copied " << GBPerSecond << " GB/s.\n";
